1d0007f2b28cc88b5ab160375921d4d2fb3beda7
Beymer Bustamante <bustamantef@unprg.edu.pe>
2023-11-20 15:25:09 -0500

    create note frond

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+import NoteForm from "@/components/NoteForm";
+import { useEffect } from "react";
+import { useNotes } from "@/context/NoteContext";
+
+// interface Note {
+//   id: number;
+//   title: string;
+//   content: string;
+// }
 
 function HomePage() {
-  return <div>HomePage</div>;
+  // const notes = await loadNotes();
+  const { notes, loadNotes } = useNotes();
+  useEffect(() => {
+    loadNotes();
+  }, []);
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        {" "}
+        <NoteForm />
+        {notes.map((note) => (
+          <div key={note.id} className="bg-slate-400 p-4 my-2">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ src/components/NoteForm.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState } from "react";
+import { useNotes } from "@/context/NoteContext";
+export default function NoteForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const { createNote } = useNotes();
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await createNote({
+          title,
+          content,
+        });
+      }}
+    >
+      <input
+        type="text"
+        name="title"
+        autoFocus
+        placeholder="Title"
+        className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        name="content"
+        placeholder="content"
+        className="w-full px-4 py-2 text-black bg-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 my-2"
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
+      <button className="px-5 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-md">
+        Create
+      </button>
+    </form>
+  );
+}
